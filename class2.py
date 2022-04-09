c23efe836fe01d77a0fcd59d015eb0389c86a987
sangsP <sangsu9701@naver.com>
2022-04-09 12:16:50 +0900

    class_2
    리스트형 공부

---
+++ class2.py
@@ -0,0 +1,19 @@
+# Chater03-3
+# 파이썬 리스트
+# 자료구조에서 중요
+# 리스트 자료형(쑨서 O, 중복 O, 수정 O, 삭제 O()
+
+# 선언
+a = []
+b = list()
+c = [70, 75, 80, 85] # len
+d = [1000, 10000, 'Ace', 'Base', 'Captain']
+e = [1000, 10000, ['Ace', 'Base', 'Captain']]
+f = [21.42, 'foobar', 3, 4, False, 3.141592]
+
+# Indexing
+print('>>>>>>')
+print('d - ', type(d), d)
+print('d - ', d[0] + d[1], d[2])
+print('d - ', d[-1])
+print('d - ', e[-1][1])
