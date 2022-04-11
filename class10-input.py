4f819378ec6fb86e870190861be017147780e053
sangsP <sangsu9701@naver.com>
2022-04-11 13:34:55 +0900

    Create class10-input.py
    사용자 입력 공부

---
+++ class10-input.py
@@ -0,0 +1,11 @@
+# Chapter 05-2
+# 파이썬 사용자 입력
+# Input 사용법
+# 기본 타임(str)
+
+# 예제 1
+name = input("Enter Your Name : ")
+grade = input("Enter Your Grade : ")
+company = input("Enter Your Company name : ")
+
+print(name, grade, company)
